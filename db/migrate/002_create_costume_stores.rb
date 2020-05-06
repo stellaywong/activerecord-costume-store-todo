5e43ea524228f0d751f944bcb62e526db8c1294c
stellaywong <stellayinwong@gmail.com>
2020-05-06 18:08:25 -0400

    Fill out migration

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+class CostumeStores < ActiveRecord::Migration
+    def change
+        create_table :costume_stores do |t|
+            t.string :name
+            t.string :location
+            t.integer :costume_inventory
+            t.integer :num_of_employees
+            # t.binary :still_in_business
+
+        end
+    end
+end
